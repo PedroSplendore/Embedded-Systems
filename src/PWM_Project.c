01d0dc6f5142b54e17759c8fffd4843cbe73818f
pedro <pedro>
2020-03-23 17:20:56 -0300

    Trying to use peron's booster pack and apply the pwm signal

--- src/PWM_Project.c
+++ src/PWM_Project.c
@@ -1,32 +1,52 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 // includes da biblioteca driverlib
 #include "inc/hw_memmap.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
 
-
 uint8_t LED_D1 = 0;
 
 void SysTick_Handler(void){
-  LED_D1 ^= GPIO_PIN_1; // Troca estado do LED D1
-  GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, LED_D1); // Acende ou apaga LED D1
-  GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, LED_D1);
-  
+//  LED_D1 ^= GPIO_PIN_1; // Troca estado do LED D1
+//  GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, LED_D1); // Acende ou apaga LED D1
 } // SysTick_Handler
 
 void main(void){
-  int i = 0;
   SysTickPeriodSet(12000000); // f = 1Hz para clock = 24MHz
   
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)); // Aguarda final da habilitação
   
+  
   GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1); // LEDs D1 e D2 como saída
   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0); // LEDs D1 e D2 apagados
   GPIOPadConfigSet(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+  
+ //////////// 
+ 
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)); // Aguarda final da habilitação
+  
+  GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5); // LEDs D3 e D4 como saída
+  GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 1); // LEDs D3 e D4 apagados
+  
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOM)); // Aguarda final da habilitação
+  
+  GPIOPinTypeGPIOOutput(GPIO_PORTM_BASE, GPIO_PIN_6); // LEDs D3 e D4 como saída
+  GPIOPinWrite(GPIO_PORTM_BASE, GPIO_PIN_6, 1); // LEDs D3 e D4 apagados
 
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOQ); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOQ)); // Aguarda final da habilitação
+  
+  GPIOPinTypeGPIOOutput(GPIO_PORTQ_BASE, GPIO_PIN_0 | GPIO_PIN_1); // LEDs D3 e D4 como saída
+  GPIOPinWrite(GPIO_PORTQ_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0); // LEDs D3 e D4 apagados
+  
+  ///////////////Q
+  
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D3 = PF4, LED D4 = PF0)
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitação
     
@@ -43,26 +63,27 @@ void main(void){
   SysTickIntEnable();
   SysTickEnable();
 
+  int i = 0;
+  GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0); // Acende LED D1
+  //LED D1 - PN1
+  //LED D2 - PN0
+  //LED D3 - PF4
+  //LED D4 - PF0
   while(1){
-    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == GPIO_PIN_0) // Testa estado do push-button SW1
-      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0); // Apaga LED D3
-    else
-      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4); // Acende LED D3
-
-    for(i=0;i<700000;i++){
-      if (i<350000){
+    for (i=0;i<100;i++){
+      if(i<99)
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0); // Apaga LED D4
-      }
-      else{
+      else 
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0); // Acende LED D4
-      }
+      if (i<98)
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0); // Apaga LED D3
+    //if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) == GPIO_PIN_1) // Testa estado do push-button SW2
+      else
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4); // Acende LED D3
+      
     }
-    i = 0;
+    i=0;
+    printf ("to aq1\n");
     
-//    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) == GPIO_PIN_1) // Testa estado do push-button SW2
-//      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0); // Apaga LED D4
-//    else
-//      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0); // Acende LED D4
   } // while
 } // main
-
